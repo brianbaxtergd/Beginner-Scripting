bc5c2e8e7e8c5839d08678c6c3644c16e0de04bc
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-07 16:36:28 -0500

    Enumerations.

---
+++ Beginner Scripting/Assets/Scripts/26 Enumerations/CardinalDirection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardinalDirection : MonoBehaviour
+{
+    enum Direction /*: short*/
+    {
+        North,
+        East,
+        South,
+        West
+    };
+
+
+    void Start()
+    {
+        Direction myDirection;
+
+        myDirection = Direction.North;
+    }
+
+    Direction ReverseDirection(Direction dir)
+    {
+        if (dir == Direction.North)
+            dir = Direction.South;
+        else if (dir == Direction.South)
+            dir = Direction.North;
+        else if (dir == Direction.East)
+            dir = Direction.West;
+        else if (dir == Direction.West)
+            dir = Direction.East;
+
+        return dir;
+    }
+}
