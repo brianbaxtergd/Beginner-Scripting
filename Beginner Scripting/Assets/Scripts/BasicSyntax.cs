2e60255e6c552b51af6a458d66e26719f915dc4a
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-07 10:21:05 -0500

    Conventions and syntax. Wrote a script reviewing basic syntax.

---
+++ Beginner Scripting/Assets/Scripts/BasicSyntax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicSyntax : MonoBehaviour
+{
+    void Start()
+    {
+        // This line is there to tell me the X position of my object.
+
+        /* Hi there!
+         * This is two lines!
+         * */
+        Debug.Log(transform.position.x);
+
+        /*
+        if (transform.position.y <= 5f)
+        {
+            Debug.Log("I'm about to the hit the ground!");
+        }
+        */
+    }
+}
