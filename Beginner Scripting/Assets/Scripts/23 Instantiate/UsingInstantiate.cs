7c72968464ce3fb0b9fcf5ac1c5dc1950771aaef
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-07 14:11:57 -0500

    Instantiate.

---
+++ Beginner Scripting/Assets/Scripts/23 Instantiate/UsingInstantiate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsingInstantiate : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public Transform firePosition;
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody projectileInstance;
+            projectileInstance = Instantiate(projectile, firePosition.position, firePosition.rotation) as Rigidbody;
+            projectileInstance.AddForce(firePosition.forward * 350f);
+        }    
+    }
+}
