368fc52e85f46b6f0dbb99a0676718a5738dccdc
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-07 16:22:49 -0500

    Arrays.

---
+++ Beginner Scripting/Assets/Scripts/24 Arrays/Arrays.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour
+{
+    //int[] myIntArray = new int[5];
+    //int[] myIntArray = { 12, 76, 8, 937, 903 };
+
+    public GameObject[] players;
+
+
+    void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            Debug.Log("Player Number " + i + " is named " + players[i].name);
+        }
+    }
+}
