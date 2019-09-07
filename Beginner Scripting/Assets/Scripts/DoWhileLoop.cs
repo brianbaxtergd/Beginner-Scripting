705c3a6f2bc9e502bbcec016686f77bebbb309e5
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-07 10:39:03 -0500

    Loops. Wrote scripts to test while, do while, for, and for each loops.

---
+++ Beginner Scripting/Assets/Scripts/DoWhileLoop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoWhileLoop : MonoBehaviour
+{
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            // Body of doWhile loop is guaranteed to run at least once.
+            print("Hello World");
+        }
+        while (shouldContinue == true); // Note semicolon at end of doWhile loop.
+    }
+}
---
+++ Beginner Scripting/Assets/Scripts/ForEachLoop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForEachLoop : MonoBehaviour
+{
+    void Start()
+    {
+        string[] strings = new string[3];
+
+        strings[0] = "First String";
+        strings[1] = "Second String";
+        strings[2] = "Third String";
+
+        foreach(string item in strings)
+        {
+            print(item);
+        }
+    }
+}
