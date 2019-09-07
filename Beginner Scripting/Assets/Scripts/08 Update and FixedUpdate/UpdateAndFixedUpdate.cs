c0cbe0f818b2c8c0f19abc34445a8227e9a07b61
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-07 11:20:26 -0500

    Update and FixedUpdate.

---
+++ Beginner Scripting/Assets/Scripts/08 Update and FixedUpdate/UpdateAndFixedUpdate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateAndFixedUpdate : MonoBehaviour
+{
+    private float fixedUpdateTimer;
+    private float updateTimer;
+
+
+    // ctrl + shift + M: Open MonoBehavior Scripting Wizard.
+
+    void FixedUpdate()
+        // Called every physics step.
+        // FixedUpdate intervals are consistent.
+        // Used for regular updates such as:
+        //  Adjusting physics (Rigidbody) objects.
+    {
+        Debug.Log("FixedUpdate time: " + Time.deltaTime);
+    }
+
+    void Update()
+        // Called every frame.
+        // Used for regular updates such as:
+        //  Moving non-physics objects.
+        //  Simple timers.
+        //  Receiving input.
+
+        // Update interval times vary.
+    {
+        Debug.Log("Update time: " + Time.deltaTime);
+    }
+}
