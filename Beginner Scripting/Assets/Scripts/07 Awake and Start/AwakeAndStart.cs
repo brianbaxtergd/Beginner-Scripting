7ad5976beb942783dc2c8cada98c33e381255f61
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-07 11:06:17 -0500

    Awake and start. Wrote script to review when Awake() and Start() functions are called.

---
+++ Beginner Scripting/Assets/Scripts/07 Awake and Start/AwakeAndStart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    // Awake() and Start() are only called once in the lifetime of a script attached to an object. Cannot repeat calls using enable/disable.
+
+
+    void Awake()
+        // References between scripts, initialization.
+        // Awake() is called even if the script component is disabled.
+        // Example: Set ammo for the enemy.
+    {
+        Debug.Log("Awake called.");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+        // Only called if script component is enabled.
+        // Start() is called ONCE if a script compopent is enabled any time while running.
+        // Example: Allow enemy to shoot.
+    {
+        Debug.Log("Start called.");
+    }
+}
