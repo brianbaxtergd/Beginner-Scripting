4c701b5ccb86b94e54094859f960821b0f5ccf90
Brian Baxter <brianbaxtergd@gmail.com>
2019-09-07 13:46:25 -0500

    Classes.

---
+++ Beginner Scripting/Assets/Scripts/22 Classes/Inventory.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public class Stuff
+    {
+        public int projectileA;
+        public int projectileB;
+        public int projectileC;
+        public float fuel;
+
+        public Stuff()
+        {
+            projectileA = 1;
+            projectileB = 1;
+            projectileC = 1;
+        }
+
+        public Stuff(int prA, int prB, int prC)
+        {
+            projectileA = prA;
+            projectileB = prB;
+            projectileC = prC;
+        }
+
+        public Stuff(int prA, float fu)
+        {
+            projectileA = prA;
+            fuel = fu;
+        }
+    }
+
+
+    public Stuff myStuff = new Stuff(50, 5, 5);
+    public Stuff myOtherStuff = new Stuff(50, 1.5f);
+
+
+    void Start()
+    {
+        Debug.Log(myStuff.projectileA);
+    }
+}
